13c75fa2148e4d568398cb3d5b433684a41babb7
linh190426 <lenhatlinhqb@gmail.com>
2026-02-24 07:49:58 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import coffee.Coffee;
+import coffee.Drink;
+import coffee.FruitJuice;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Drink[] drinks = new Drink[3];
-        drinks[0] = new Coffee("DR001", "Bạc sỉu", 30000, true);
-        drinks[1] = new FruitJuice("DR002", "Nước cam", 40000, 10);
+        drinks[0] = new Coffee(1, "Bạc sỉu", 30000, true);
+        drinks[1] = new FruitJuice(2, "Nước cam", 40000, 10);
 
         for (Drink drink : drinks) {
             if (drink != null){
